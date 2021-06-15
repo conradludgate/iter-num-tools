abd1dc04533489a45df6901cbfeb955f508c73d5
Conrad Ludgate <conradludgate@gmail.com>
2021-06-15 11:45:38 +0100

    update benchmark

--- benches/linspace.rs
+++ benches/linspace.rs
@@ -1,31 +1,34 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use iter_num_tools::lin_space;
 
+fn bench(i: impl Iterator<Item=f64>) -> Vec<f64> {
+    black_box(i.map(|x| x * 2.0).collect())
+}
+
 fn lin_space_std(start: f64, end: f64, steps: usize) -> impl Iterator<Item = f64> {
-    let step = steps as f64;
     let len = end - start;
-    (0..steps).map(move |i| start + i as f64 / step * len)
+    let step = len / steps as f64;
+    (0..=steps).map(move |i| start + i as f64 * step)
 }
 
 pub fn bench_lin_space(c: &mut Criterion) {
     let mut group = c.benchmark_group("LinSpace");
 
-    group.bench_function("linspace [1.0, 3.0) x100", |b| {
+    group.bench_function("linspace [1.0, 3.0] x100 (iter-num-tools)", |b| {
         b.iter(|| {
-            black_box(
-                lin_space(1.0..3.0, 100)
-                    .map(|x| x * 2.0)
-                    .collect::<Vec<f64>>(),
-            )
+            bench(lin_space(1.0..=3.0, 100))
         })
     });
-    group.bench_function("linspace [1.0, 3.0) x100 std", |b| {
+
+    group.bench_function("linspace [1.0, 3.0] x100 (std)", |b| {
         b.iter(|| {
-            black_box(
-                lin_space_std(1.0, 3.0, 100)
-                    .map(|x| x * 2.0)
-                    .collect::<Vec<f64>>(),
-            )
+            bench(lin_space_std(1.0, 3.0, 100))
+        })
+    });
+
+    group.bench_function("linspace [1.0, 3.0] x100 (itertools-num)", |b| {
+        b.iter(|| {
+            bench(itertools_num::linspace(1.0, 3.0, 100))
         })
     });
 
@@ -34,7 +37,3 @@ pub fn bench_lin_space(c: &mut Criterion) {
 
 criterion_group!(benches, bench_lin_space);
 criterion_main!(benches);
-
-// n = (steps as F)
-// x = (i as F) / n
-// y0 + x * (y1 - y0)
